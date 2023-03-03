953d24d32d10fa9c64fb1b0762c9a572b202c285
LUIS <luis.fernandez@integralsoft.co>
2023-03-03 15:42:32 -0500

    Nuevos cambios

--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -1,11 +1,20 @@
+/**creacion de metodos para la busqueda en la api */
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Character } from '@app/components/pages/interfaces/character.interface';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  searchCharacters(query = '', page = 1) {}
-  getDetails(id: number) {}
+  searchCharacters(query = '', page = 1) {
+    const filter = `${environment.baseUrlAPI}/?name=${query}&page=${page}`;
+    return this.http.get<Character[]>(filter);
+  }
+  getDetails(id: number) {
+    return this.http.get<Character>(`${environment.baseUrlAPI}/${id}`);
+  }
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  baseUrlAPI: 'https://rickandmortyapi.com/api/character',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDQC4musm-huU2SqQowI58YNduM37BUdnM',
+    authDomain: 'likeutest-4674c.firebaseapp.com',
+    projectId: 'likeutest-4674c',
+    storageBucket: 'likeutest-4674c.appspot.com',
+    messagingSenderId: '1020529717232',
+    appId: '1:1020529717232:web:2df8307282f3dfea107735',
+    measurementId: 'G-5TYSSSWYBT',
+  },
+};
